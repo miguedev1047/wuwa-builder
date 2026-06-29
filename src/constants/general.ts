ad6311e114576e63eb519d12c80aeb8ac2aad667
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-06-28 19:36:54 -0500

    feat: add general constants

---
+++ src/constants/general.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = 'Wuwa Builds'
+export const APP_VERSION = '0.0.1'
+export const DEFAULT_IMAGE = 'https://cdn.wuwaflex.com/img/stats/ico-lock.png'
+
+export const LOCAL_URL = 'http://localhost:3000'
+export const PRODUCTION_URL = 'https://wuwa-builds.appma.workers.dev'
+
+export const DEFAULT_DESCRIPTION = { type: 'doc', content: [] }
