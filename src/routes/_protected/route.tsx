b02afc3b05f577492468ca090f552a68d104fe0e
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-06-29 07:55:44 -0500

    feat: add _protected layout

---
+++ src/routes/_protected/route.tsx
@@ -0,0 +1,19 @@
+import { getSession } from '#/functions/auth.functions'
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/_protected')({
+  beforeLoad: async ({ location }) => {
+    const session = await getSession()
+
+    if (!session) {
+      throw redirect({ to: '/auth/login', search: { redirect: location.href } })
+    }
+
+    return { user: session.user }
+  },
+  component: () => (
+    <div className="p-8">
+      <Outlet />
+    </div>
+  ),
+})
