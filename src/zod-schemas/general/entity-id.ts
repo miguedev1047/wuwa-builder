6fdb18c5754a935677cdc91f9267c463e76d1490
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-06-28 20:32:16 -0500

    chore: rename the entity schema

--- src/zod-schemas/general/entity-id.ts
+++ src/zod-schemas/general/entity-id.ts
@@ -1,9 +1,9 @@
 import z from 'zod'
 
-export const entityIdZodSchema = z.object({
+export const entityIdZod = z.object({
   id: z
     .string({ error: 'Campo id inválido' })
     .min(1, { error: 'El campo ID es requerido' }),
 })
 
-export type TEntityIdZod = z.infer<typeof entityIdZodSchema>
+export type TEntityIdZod = z.infer<typeof entityIdZod>
--- src/zod-schemas/images/resonators.ts
+++ src/zod-schemas/images/resonators.ts
@@ -1,9 +1,9 @@
 import { imageRemoveZodSchema } from '#/zod-schemas/images/remove'
 import { imageUploadZodSchema } from '#/zod-schemas/images/upload'
-import { entityIdZodSchema } from '#/zod-schemas/general/entity-id'
+import { entityIdZod } from '#/zod-schemas/general/entity-id'
 
 export const resonatorImageUploadZodSchema =
-  imageUploadZodSchema.extend(entityIdZodSchema)
+  imageUploadZodSchema.extend(entityIdZod)
 
 export const resonatorImageRemoveZodSchema =
-  imageRemoveZodSchema.extend(entityIdZodSchema)
+  imageRemoveZodSchema.extend(entityIdZod)
