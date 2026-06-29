2a9e196d63a00901991d8ebbf457b46fa4c9b58c
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-06-28 19:34:16 -0500

    chore: minor changes

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,9 +1,18 @@
-import { cn } from "#/lib/utils.ts"
-import { Loader2Icon } from "lucide-react"
+import { cn } from '#/lib/utils.ts'
+import { RiLoaderLine } from '@remixicon/react'
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({
+  className,
+  ...props
+}: React.ComponentProps<typeof RiLoaderLine>) {
   return (
-    <Loader2Icon data-slot="spinner" role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
+    <RiLoaderLine
+      data-slot="spinner"
+      role="status"
+      aria-label="Loading"
+      className={cn('size-4 animate-spin', className)}
+      {...props}
+    />
   )
 }
 
