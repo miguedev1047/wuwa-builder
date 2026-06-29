f6b979b7e2d875be829ab18c905f7755c1097ac0
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-06-28 19:37:05 -0500

    feat: add resonators constants

---
+++ src/constants/resonators.ts
@@ -0,0 +1,133 @@
+export const ResonatorRolesEnum = [
+  'support',
+  'healer',
+  'sub_dps',
+  'dps',
+] as const
+export const RESONATOR_ROLES = [
+  {
+    label: 'Soporte',
+    value: 'support',
+  },
+  {
+    label: 'Healer',
+    value: 'healer',
+  },
+  {
+    label: 'Sub DPS',
+    value: 'sub_dps',
+  },
+  {
+    label: 'DPS',
+    value: 'dps',
+  },
+]
+
+export const ResonatorElementsEnum = [
+  'aero',
+  'fusion',
+  'glacio',
+  'electro',
+  'spectro',
+  'havoc',
+] as const
+export const RESONATOR_ELEMENTS = [
+  {
+    label: 'Aero',
+    value: 'aero',
+  },
+  {
+    label: 'Fusion',
+    value: 'fusion',
+  },
+  {
+    label: 'Glacio',
+    value: 'glacio',
+  },
+  {
+    label: 'Electro',
+    value: 'electro',
+  },
+  {
+    label: 'Espectro',
+    value: 'spectro',
+  },
+  {
+    label: 'Destrucción',
+    value: 'havoc',
+  },
+]
+
+export const RESONATOR_SKILLS = [
+  {
+    label: 'Circuito Forte',
+    value: 'forte',
+    order: 0,
+  },
+  {
+    label: 'Ataque normal',
+    value: 'normal_attack',
+    order: 1,
+  },
+  {
+    label: 'Habilidad de resonancia',
+    value: 'resonance_skill',
+    order: 2,
+  },
+  {
+    label: 'Lib. de resonancia',
+    value: 'ultimate_skill',
+    order: 3,
+  },
+  {
+    label: 'Intro',
+    value: 'intro_skill',
+    order: 4,
+  },
+  {
+    label: 'Outro',
+    value: 'outro_skill',
+    order: 5,
+  },
+]
+
+export const ResonanceChainType = [
+  'resonance_chain_1',
+  'resonance_chain_2',
+  'resonance_chain_3',
+  'resonance_chain_4',
+  'resonance_chain_5',
+  'resonance_chain_6',
+] as const
+export const RESONATOR_RESONANCE_CHAINS = [
+  {
+    label: 'Resonancia 1',
+    value: 'resonance_chain_1',
+    order: 1,
+  },
+  {
+    label: 'Resonancia 2',
+    value: 'resonance_chain_2',
+    order: 2,
+  },
+  {
+    label: 'Resonancia 3',
+    value: 'resonance_chain_3',
+    order: 3,
+  },
+  {
+    label: 'Resonancia 4',
+    value: 'resonance_chain_4',
+    order: 4,
+  },
+  {
+    label: 'Resonancia 5',
+    value: 'resonance_chain_5',
+    order: 5,
+  },
+  {
+    label: 'Resonancia 6',
+    value: 'resonance_chain_6',
+    order: 6,
+  },
+]
