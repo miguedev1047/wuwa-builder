6409c60aacd23f3b1c26910037d007f713a74958
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-06-29 09:26:25 -0500

    chore: update prod. url

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -3,6 +3,6 @@ export const APP_VERSION = '0.0.1'
 export const DEFAULT_IMAGE = 'https://cdn.wuwaflex.com/img/stats/ico-lock.png'
 
 export const LOCAL_URL = 'http://localhost:3000'
-export const PRODUCTION_URL = 'https://wuwa-builds.appma.workers.dev'
+export const PRODUCTION_URL = 'https://wuwa-builder.appma.workers.dev/'
 
 export const DEFAULT_DESCRIPTION = { type: 'doc', content: [] }
