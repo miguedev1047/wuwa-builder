4b080a42c8f7751d2425511c6848ca3f7bafe016
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-06-29 22:45:08 -0500

    feat: add weapons orpc router

--- src/integrations/orpc/router.ts
+++ src/integrations/orpc/router.ts
@@ -1,5 +1,7 @@
 import { resonatorsRouter } from '#/integrations/orpc/routers/resonators'
+import { weaponsRouter } from '#/integrations/orpc/routers/weapons'
 
 export const appRouter = {
   resonators: resonatorsRouter,
+  weapons: weaponsRouter,
 }
