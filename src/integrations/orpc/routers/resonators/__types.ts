37df4d10b64f49d552799d7ddff22343ded9919c
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-06-30 23:43:27 -0500

    chore(update): add all resonator entity types

--- src/integrations/orpc/routers/resonators/__types.ts
+++ src/integrations/orpc/routers/resonators/__types.ts
@@ -11,3 +11,15 @@ export type TResonatorAssetTable = TResonatorAssetsTable[number]
 
 export type TResonatorBestWeaponsTable = TResonatorTable['best_weapons']
 export type TResonatorBestWeaponTable = TResonatorBestWeaponsTable[number]
+
+export type TResonatorLevelsTable = TResonatorTable['levels']
+export type TResonatorLevelTable = TResonatorLevelsTable[number]
+
+export type TResonatorSkillsTable = TResonatorTable['skills']
+export type TResonatorSkillTable = TResonatorSkillsTable[number]
+
+export type TResonatorSequencesTable = TResonatorTable['sequences']
+export type TResonatorSequenceTable = TResonatorSequencesTable[number]
+
+export type TResonatorBonusesTable = TResonatorTable['bonuses']
+export type TResonatorBonusTable = TResonatorBonusesTable[number]
